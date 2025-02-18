4bc99d448c9e031d457bf960dbbe76aa3e6c797a
ellenaron <ellaro093@hengersor.hu>
2025-02-18 16:47:54 +0100

    Main elkezdve
    Ebben a weboldal css alapbeállításai készültek el, illetve elkezdtem a main-t.

--- js/navbar.js
+++
@@ -1,9 +0,0 @@
-const collapseButton = document.getElementById("collapse-button");
-const navbar = document.getElementById("navbar");
-
-collapseButton.addEventListener("click", () => collapeToggle());
-
-function collapeToggle() {
-    navbar.classList.toggle("open");
-    collapseButton.classList.toggle("active");
-}
